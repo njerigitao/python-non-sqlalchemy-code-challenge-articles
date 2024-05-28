52369d3a41b79a647616de84ca0c6dba9d92718d
AGNESGITAU <agnesgitau40@gmail.com>
2024-05-28 16:04:58 +0300

    instantiate name, and category in magazine class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -24,3 +24,22 @@ class Author:
             return article
         def magazines(self):
             return list(set([article.magazine for article in self._articles]))
+        def topic_areas(self):
+            magazines = self.magazines()
+            if not magazines:
+                return None
+            return list(set([magazine.category for magazine in magazines]))
+class Magazine:
+    def __init__(self, name, category):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if len(name) < 2 or len(name) > 16:
+            raise ValueError("Name must be between 2 and 16 characters")
+        if not isinstance(category, str):
+            raise TypeError("Category must be a string")
+        if len(category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._name = name
+        self._category = category
+        self._articles = []
+        
