ae150ae9f86dbc6a907dbfa5d6172f59e92e03c7
AGNESGITAU <agnesgitau40@gmail.com>
2024-05-28 16:37:06 +0300

    Re initialize commits

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,52 +1,77 @@
+class Article:
+    def __init__(self, author, magazine, title):
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+
+        @property
+        def author(self):
+            return self.__author
+        
+        @property
+        def magazine(self):
+            return self.__magazine
+
+        @property
+        def title(self):
+            return self.__title
+        
 class Author:
     def __init__(self, name):
         if not isinstance(name, str):
-            raise TypeError("Name must be a string")
-        if len(name) == 0:
-            raise ValueError("Name must be longer than 0 characters")
-        self._name = name
+            raise ValueError("Author name must be a string")
+        self.name = name
         self._articles = []
 
         @property
         def name(self):
-            return self._name
-        def articles(self):
-            return self._articles
-        def add_article(self, magazine, title):
-            if not isinstance(magazine, Magazine):
-                raise TypeError("Magazine must be an instance of Magazine")
-            if not isinstance(title, str):
-                raise TypeError("Title must be a string")
-            if len(title) < 5 or len(title) > 50:
-                raise ValueError("Title must be between 5 and 50 characters")
-            article = Article(self, magazine, title)
-            self._articles.append(article)
-            return article
-        def magazines(self):
-            return list(set([article.magazine for article in self._articles]))
-        def topic_areas(self):
-            magazines = self.magazines()
-            if not magazines:
-                return None
-            return list(set([magazine.category for magazine in magazines]))
+            return self.__name
+
+    def articles(self):
+        return self._articles
+
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
+
+    def add_article(self, magazine, title):
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
+
+    def topic_areas(self):
+        return list(set(article.magazine.category for article in self._articles))
+
 class Magazine:
     def __init__(self, name, category):
         if not isinstance(name, str):
-            raise TypeError("Name must be a string")
-        if len(name) < 2 or len(name) > 16:
-            raise ValueError("Name must be between 2 and 16 characters")
-        if not isinstance(category, str):
-            raise TypeError("Category must be a string")
-        if len(category) == 0:
-            raise ValueError("Category must be longer than 0 characters")
-        self._name = name
-        self._category = category
+            raise ValueError("Magazine name must be a string")
+        if not isinstance(category, str) or len(category) == 0:
+            raise ValueError("Magazine category must be a non-empty string")
+        self.__name = name
+        self.__category = category
         self._articles = []
 
         @property
         def name(self):
-            return self._name
+            return self.__name
         @property
         def category(self):
-            return self._category
+            return self.__category
+
+    def articles(self):
+        return self._articles
+
+    def contributors(self):
+        return list(set(article.author for article in self._articles))
+
+    def article_titles(self):
+        return[article.title for article in self._articles]
+
+    def contributing_authors(self):
+        authors = {}
+        for article in self._articles:
+            author = article.author
+            authors[author] = authors.get(author,0)+ 1
+            return[author for author, count in authors.items() if count > 2]
+
         
